2a1700f8acaea5d355207cfe2d5d817d0a416877
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2021-09-23 01:46:43 +0300

    Add files via upload

---
+++ task5/src/task5/testDog.java
@@ -0,0 +1,12 @@
+package task5;
+
+public class testDog {
+
+ public static void main(String[] args) {
+ 
+  Dachshund d = new Dachshund("Рада", "рыжая");
+  d.outPut();
+  Corgi c = new Corgi("Яни","белая");
+  c.outPut();
+}
+}
---
+++ task5/src/task5/testFurniture.java
@@ -0,0 +1,19 @@
+package task5;
+public class testFurniture {
+
+public static void main(String[] args) {
+ 
+  Chair c = new Chair("Берёза", "Россия", 7);
+  Table t = new Table("Дуб","Швеция", 13);
+  FurnitureShop buyC = new FurnitureShop("Стол","Дуб","Швеция",1);
+  FurnitureShop buyT = new FurnitureShop("Стул","Берёза","Россия",4);
+  FurnitureShop not = new FurnitureShop("Кровать","Дуб","Швеция",1);
+  buyC.check();
+  buyT.check();
+  not.check();
+  System.out.println("Зато у нас есть: \n");
+  c.outPut();
+  t.outPut();
+
+}
+}
