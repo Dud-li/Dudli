3d429eaf7227b2caaefb974f7397bb39e2cb44ca
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2021-09-23 00:25:30 +0300

    Add files via upload

---
+++ task2/Shape.java
@@ -0,0 +1,34 @@
+package task;
+
+public class Shape {
+    private String color;
+    private int numofvert;
+
+    public Shape(String a, int b) {
+        color = a;
+        numofvert = b;
+    }
+    public Shape(String dn){
+        color = dn;
+        numofvert = 0;
+    }
+    public Shape(int sz){
+        color = "Red";
+        numofvert = sz;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setNumofvert(int numofvert) {
+        this.numofvert = numofvert;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getNumofvert(){
+        return numofvert;
+    }
+    public String toString(){
+        return "Shape of "+this.color +" color has "+this.numofvert+" vertices";
+    }
+}
