1d68d3a00b25041e334f10a6218fbd96dac33b04
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2021-09-23 01:08:31 +0300

    Add files via upload

---
+++ task4/TestAuthor.java
@@ -0,0 +1,8 @@
+package pj2;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author a = new Author("Harry Potter","abc@mail.ru",'M');
+        System.out.println(a);
+    }
+}
