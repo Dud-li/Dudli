46efd1c08a2ce405f988e1c1d70a9443699c989b
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2021-09-29 17:29:07 +0300

    Delete task3 directory

--- task3/src/task3/testHuman.java
+++
@@ -1,8 +0,0 @@
-package task3;
-
-public class testHuman {
- public static void main(String[] args){
-        Human h = new Human("блондин","худые","худые");
-        System.out.println(h);
-    }
-}
