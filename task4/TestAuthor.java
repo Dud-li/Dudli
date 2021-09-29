f6c2bc840c62c49f71cabab10f671c43aa998bf4
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2021-09-29 17:29:16 +0300

    Delete task4 directory

--- task4/TestAuthor.java
+++
@@ -1,8 +0,0 @@
-package pj2;
-
-public class TestAuthor {
-    public static void main(String[] args){
-        Author a = new Author("Harry Potter","abc@mail.ru",'M');
-        System.out.println(a);
-    }
-}
